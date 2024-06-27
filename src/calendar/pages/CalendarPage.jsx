df6f13fa233c87da289b8c7450c0af5b78ae9cfd
mathiasur <mathias.urtheil@gmail.com>
2024-06-27 12:42:21 -0300

    Add react-big-calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,46 @@
-import { NavBar } from '../components/NavBar';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { format, addHours, parse, startOfWeek, getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US'
+
+import { NavBar } from '../';
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Reunión con el gerente',
+  notes: 'Preparar la presentación',
+  start: new Date(),
+  end: addHours(new Date(), 1),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Mathias'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <NavBar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px' }}
+      />
     </>
   )
 }
