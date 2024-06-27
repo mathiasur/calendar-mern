5b6f28664f015f296ad30ca1c41a472b818eb84a
mathiasur <mathias.urtheil@gmail.com>
2024-06-26 22:36:40 -0300

    Add navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,7 +6,7 @@ import { CalendarPage } from '../calendar/pages/CalendarPage';
 
 export const AppRouter = () => {
 
-  const authStatus = 'not-authenticated';
+  const authStatus = 'authenticated';
 
   return (
     <Routes>
