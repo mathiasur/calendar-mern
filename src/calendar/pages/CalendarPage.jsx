8261a1ac8695e2c749da7b547f6d67a9244ac498
mathiasur <mathias.urtheil@gmail.com>
2024-06-28 00:13:25 -0300

    Add calendar event listeners

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { addHours } from 'date-fns';
@@ -19,6 +20,8 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
   const eventStyleGetter = (event, start, end, isSelected) => {
 
     const style = {
@@ -33,6 +36,18 @@ export const CalendarPage = () => {
     }
   }
 
+  const onDoubleClick = (event) => {
+    console.log({onDoubleClickEvent: event});
+  }
+
+  const onSelect = (event) => {
+    console.log({onSelect: event});
+  }
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+  }
+
   return (
     <>
       <NavBar />
@@ -41,6 +56,7 @@ export const CalendarPage = () => {
         culture="es"
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px' }}
@@ -49,6 +65,9 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
     </>
   )
